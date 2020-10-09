dfddd25c305a197dd8dd3b7cf17353da86f70e2e
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-08 20:27:04 -0500

    started the beginnings of the class work. Need to complete

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000
-
-const { users } = require('./state')
+const { users } = require("./state");
 
 /* BEGIN - create routes here */
 
+// We are now getting all of the users
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+//  We now want a single user
+app.post("/users/1", (req, res) => {
+  let user = { _id: 5, Name: "Sampson", Occupation: "Attourney" };
+  // add the user to the array
+  users.push(user);
+  res.send(user);
+});
+
+// We want to make a put request to mutate an object
+app.put("/users", (req, res) => {
+  res.send(users);
+});
 
 /* END - create routes here */
 
-app.listen(port, () => 
-  console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
