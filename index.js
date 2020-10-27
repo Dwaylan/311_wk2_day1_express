4d94d4d267dcac378ffb075c6f86e76a7c52fd22
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-10-27 13:19:35 -0500

    well that was interesting to say the least, something tells me that I meant to go back to the lesson and do this code step by step but forgot. It's clean now

--- index.js
+++ index.js
@@ -17,27 +17,23 @@ app.get("/users/:userId", (req, res) => {
   res.send(user);
 });
 
-// We path for a post request!
+// We want to post a new user!
 app.post("/users", (req, res) => {
-  console.log(req.body);
-  res.send(users[parseInt(req.params.userId) - 1]);
-
+  // requesting the body
   let user = req.body;
+  // The new user id will be greater than the one preceeding it by 1
   user._id = user.length + 1;
-
   // The push method adds a new variable to an array
   users.push(user);
-
   // Our response is to return the users
   res.send(users);
 });
 
 // We are now using the put method to update a user
-app.put("/users/:userId", (req, res) => {
-  let userId = parseInt(req.params.userId) - 1;
-
-  //  We have to create a function to find users within the payload
-  let user = users.find((user) => user._id === userId);
+app.put("/users/", function (req, res) {
+  //  We have to create a function to place a user within the payload.
+  // We are going to make a user 0
+  let user = users[0];
 
   //  We use dot notation to access the name key and mutate the object
   user.name = "Jane";
@@ -46,9 +42,12 @@ app.put("/users/:userId", (req, res) => {
   res.send(users[0]);
 });
 
-//  We want to delete a user
+//  We want use the  delete method to change the status of a user to false by their user ID
 app.delete("/users/:userId", (req, res) => {
-  res.send(users.splice(1, 1));
+  let userId = parseInt(req.params.userId);
+  let user = user.find((user) => user._id === userId);
+  user.isActive = false;
+  res.send("delete");
 });
 
 /* END - create routes here */
